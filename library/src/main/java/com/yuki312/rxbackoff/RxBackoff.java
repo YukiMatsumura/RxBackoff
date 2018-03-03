7bab5416db182339a5336a17fd2ffd7e0b440e36
Yuki Matsumura <define1394@gmail.com>
2018-03-03 16:17:14 +0900

    Add throwable filter function

--- library/src/main/java/com/yuki312/rxbackoff/RxBackoff.java
+++ library/src/main/java/com/yuki312/rxbackoff/RxBackoff.java
@@ -6,6 +6,7 @@ import io.reactivex.ObservableSource;
 import io.reactivex.functions.BiConsumer;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
 import java.util.concurrent.TimeUnit;
 
 public class RxBackoff implements Function<Observable<Throwable>, ObservableSource<?>> {
@@ -14,6 +15,12 @@ public class RxBackoff implements Function<Observable<Throwable>, ObservableSour
   private final Function<Integer, Long> delayFunction;
 
   private int retryCount = 0;
+
+  private Predicate<Throwable> filter = new Predicate<Throwable>() {
+    @Override public boolean test(Throwable throwable) throws Exception {
+      return true;
+    }
+  };
 
   private BiConsumer<Throwable, Integer> onRetry = new BiConsumer<Throwable, Integer>() {
     @Override public void accept(Throwable e, Integer retry) throws Exception {
@@ -43,6 +50,10 @@ public class RxBackoff implements Function<Observable<Throwable>, ObservableSour
   @Override public ObservableSource apply(Observable<Throwable> attempts) throws Exception {
     return attempts.flatMap(new Function<Throwable, ObservableSource<?>>() {
       @Override public ObservableSource<?> apply(Throwable throwable) throws Exception {
+        if (!filter.test(throwable)) {
+          return Observable.error(throwable);
+        }
+
         if (++retryCount <= maxRetry) {
           onRetry.accept(throwable, retryCount);
           return Observable.timer(delayFunction.apply(retryCount), TimeUnit.MILLISECONDS);
@@ -52,6 +63,11 @@ public class RxBackoff implements Function<Observable<Throwable>, ObservableSour
         }
       }
     });
+  }
+
+  public RxBackoff filter(@NonNull Predicate<Throwable> predicate) {
+    this.filter = predicate;
+    return this;
   }
 
   public RxBackoff doOnRetry(@NonNull BiConsumer<Throwable, Integer> onRetry) {
