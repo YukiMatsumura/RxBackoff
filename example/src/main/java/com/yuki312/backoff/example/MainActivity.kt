5e1ba8825848a09b1ab027bf0184fb076b786e5f
Yuki Matsumura <define1394@gmail.com>
2018-03-09 00:16:14 +0900

    split core / rxjava2 library

---
+++ example/src/main/java/com/yuki312/backoff/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.yuki312.backoff.example
+
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity()
--- example/src/main/java/com/yuki312/rxbackoff/example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.yuki312.rxbackoff.example
-
-import android.support.v7.app.AppCompatActivity
-
-class MainActivity : AppCompatActivity()
